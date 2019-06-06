0e8df21c0e73362177f2298fe726d36ba8d82e02
Sumaer Jolly <sumaerjolly@gmail.com>
2019-06-06 23:30:21 +0530

    everything but inject done

--- enumerable-methods.rb
+++ enumerable-methods.rb
@@ -1,1 +1,47 @@
-puts "Initialize"
+module Enumerable
+    def my_each
+      for i in 0...self.length
+        yield(self[i])
+      end  
+    end
+
+    def my_each_with_index
+
+    end
+
+    def my_select
+
+    end
+
+    def my_all?
+    
+    end
+
+    def my_any?
+    
+    end
+
+    def my_none?
+    
+    end
+    
+    def my_count
+    
+    end
+
+    def my_map
+
+    end
+
+    def my_inject
+    
+    end
+
+    def multiply_els
+    
+    end
+
+
+end
+
+my_each([1,2,3,4])
